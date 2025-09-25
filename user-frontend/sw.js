3cbaa9c9b3c53ada632c52a031d45eb2d7328806
Gabriel Medina <gmedinat911@gmail.com>
2025-09-25 16:25:44 -0400

    feat: integrate OneSignal v16 web SDK into service worker

--- user-frontend/sw.js
+++ user-frontend/sw.js
@@ -1,3 +1,6 @@
+// OneSignal v16 worker integration
+try { importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js'); } catch (e) {}
+
 self.addEventListener('install', (event) => {
   // Activate updated SW immediately
   self.skipWaiting();
